d94dde5bec3b240720acb30161c045d3a52b4700
Jonathan G <jonat.garnier@free.fr>
2019-06-29 17:47:00 +0200

    Fixed changed url for 2d texture

--- src/main/java/models/TextureContainer.java
+++ src/main/java/models/TextureContainer.java
@@ -1,17 +1,11 @@
 package models;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import org.newdawn.slick.opengl.Texture;
-import org.newdawn.slick.opengl.TextureLoader;
-
 public interface TextureContainer {
-	static final Path resourceTexturePath = Paths.get("./", "resources", "2D");
+	static final Path resourceTexturePath = Paths.get("./", "src", "main", "resources", "2D");
 	public Optional<ArrayList<TextureData>> getTextures();
 }
